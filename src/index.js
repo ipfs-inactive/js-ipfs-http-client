b7595adb6e402164a28c2ec8193249182e62b592
David Dias <daviddias.p@gmail.com>
2015-10-11 09:12:08 -0700

    fix rm -r bug

--- src/index.js
+++ src/index.js
@@ -277,7 +277,6 @@ function IpfsAPI (host_or_multiaddr, port, opts) {
       return requestAPI('files/rm', path, opts, null, cb)
     },
     read: argCommand('files/read'),
-    // write: argCommand('files/write'),
     write: function (pathDst, files, opts, cb) {
       if (typeof (opts) === 'function' && cb === undefined) {
         cb = opts
