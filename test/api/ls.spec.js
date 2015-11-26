6fe1f579238217f21fdd48af0987e6382502f0b3
Enrico Fasoli <fazius2009@gmail.com>
2015-11-26 17:49:31 +0100

    updated ls test spec

--- test/api/ls.spec.js
+++ test/api/ls.spec.js
@@ -2,18 +2,25 @@
 
 describe('ls', function () {
   it('should correctly retrieve links', function (done) {
-    this.timeout(5000)
     apiClients['a'].ls('QmTDH2RXGn8XyDAo9YyfbZAUXwL1FCr44YJCN9HBZmL9Gj', (err, res) => {
-      if (err) {
-        throw err
-      } else done()
+      if (err === null && res !== undefined) {
+        assert(res.Objects[0].Links[0])
+        done()
+      }
     })
   })
-  it('should correctly throw errors', function (done) {
+  it('should correctly handle a nonexisting hash', function (done) {
     apiClients['a'].ls('surelynotavalidhashheh?', (err, res) => {
-      if (err === undefined) {
-        throw new Error()
-      } else done()
+      if (err !== null && res === undefined) {
+        done()
+      }
+    })
+  })
+  it('should correctly handle a nonexisting path', function (done) {
+    apiClients['a'].ls('QmTDH2RXGn8XyDAo9YyfbZAUXwL1FCr44YJCN9HBZmL9Gj/folder_that_isnt_there', (err, res) => {
+      if (err !== null && res === undefined) {
+        done()
+      }
     })
   })
 })
