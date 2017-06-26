34d95a89c881d80459f0e0e2491aa8b406ae0577
Nuno Nogueira <nunofmn@gmail.com>
2017-06-26 14:27:58 +0100

    examples: fix csv generation in submodules example.

--- examples/sub-module/test-modules-size.sh
+++ examples/sub-module/test-modules-size.sh
@@ -2,16 +2,16 @@
 
 modules=($(ls modules/))
 
-echo "name - bundled - minified"
+echo "name, bundled (KBs), minified (KBs)"
 
 # Full IPFS module
 webpack --display none --config webpack.config.js complete-module.js complete-bundle.js
 babili complete-bundle.js -o complete-bundle-minified.js
 
-ipfsBundleSize=($(wc -c < complete-bundle.js | awk '{b=$1/1024; printf "%.2f\n", b}'))
-ipfsMinSize=($(wc -c < complete-bundle-minified.js | awk '{b=$1/1024; printf "%.2f\n", b}'))
+ipfsBundleSize=($(wc -c < complete-bundle.js | awk '{b=$1/1024; printf "%.2f\n", b}' | sed 's/,/./g'))
+ipfsMinSize=($(wc -c < complete-bundle-minified.js | awk '{b=$1/1024; printf "%.2f\n", b}' | sed 's/,/./g'))
 
-echo IPFS - $ipfsBundleSize - $ipfsMinSize
+echo IPFS, $ipfsBundleSize, $ipfsMinSize
 
 for module in "${modules[@]}"
 do
@@ -19,7 +19,7 @@ do
   webpack --display none --config webpack.config.js $moduledir/$module.js $moduledir/bundle.js
   babili $moduledir/bundle.js -o $moduledir/bundle-minified.js
 
-  bundlesize=($(wc -c < $moduledir/bundle.js | awk '{b=$1/1024; printf "%.2f\n", b}'))
-  minsize=($(wc -c < $moduledir/bundle-minified.js | awk '{b=$1/1024; printf "%.2f\n", b}'))
-  echo $module - $bundlesize - $minsize
+  bundlesize=($(wc -c < $moduledir/bundle.js | awk '{b=$1/1024; printf "%.2f\n", b}' | sed 's/,/./g'))
+  minsize=($(wc -c < $moduledir/bundle-minified.js | awk '{b=$1/1024; printf "%.2f\n", b}' | sed 's/,/./g'))
+  echo $module, $bundlesize, $minsize
 done
