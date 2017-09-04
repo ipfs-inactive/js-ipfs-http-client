3b34a8bcbc1fb2c01ad7425bacb54f158f992522
David Dias <daviddias.p@gmail.com>
2017-09-04 13:38:31 +0100

    bring back the fixtures

--- .aegir.js
+++ .aegir.js
@@ -5,7 +5,7 @@ const factory = require('./test/ipfs-factory/tasks')
 module.exports = {
   karma: {
     files: [{
-      pattern: 'node_modules/interface-ipfs-core/src/fixtures/**/*',
+      pattern: 'node_modules/interface-ipfs-core/test/fixtures/**/*',
       watched: false,
       served: true,
       included: false
