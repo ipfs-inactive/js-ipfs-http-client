b8f324a218c6686b3dbcb5231aaf78d9ed553b6b
David Dias <daviddias.p@gmail.com>
2017-09-04 13:23:53 +0100

    fix: update fixtures location

--- .aegir.js
+++ .aegir.js
@@ -5,7 +5,7 @@ const factory = require('./test/ipfs-factory/tasks')
 module.exports = {
   karma: {
     files: [{
-      pattern: 'node_modules/interface-ipfs-core/test/fixtures/**/*',
+      pattern: 'node_modules/interface-ipfs-core/src/fixtures/**/*',
       watched: false,
       served: true,
       included: false
