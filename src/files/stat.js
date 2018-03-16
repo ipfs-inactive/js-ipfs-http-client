e99f98ea061c9892abe1d1add8da9d7f0229c337
David Dias <daviddias.p@gmail.com>
2018-03-16 11:21:39 -0700

    fix: do not use stream to value

--- src/files/stat.js
+++ src/files/stat.js
@@ -4,23 +4,23 @@ const promisify = require('promisify-es6')
 const _ = require('lodash')
 const streamToValue = require('../utils/stream-to-value')
 
-const transform = function (res, callback) {
-  return streamToValue(res, (err, data) => {
-    if (err) {
-      return callback(err)
-    }
+const transform = function (data, callback) {
+  // return streamToValue(res, (err, data) => {
+  //  if (err) {
+  //    return callback(err)
+  //  }
 
-    callback(null, {
-      type: data[0].Type,
-      blocks: data[0].Blocks,
-      size: data[0].Size,
-      hash: data[0].Hash,
-      cumulativeSize: data[0].CumulativeSize,
-      withLocality: data[0].WithLocality || false,
-      local: data[0].Local || null,
-      sizeLocal: data[0].SizeLocal || null
-    })
+  callback(null, {
+    type: data[0].Type,
+    blocks: data[0].Blocks,
+    size: data[0].Size,
+    hash: data[0].Hash,
+    cumulativeSize: data[0].CumulativeSize,
+    withLocality: data[0].WithLocality || false,
+    local: data[0].Local || null,
+    sizeLocal: data[0].SizeLocal || null
   })
+  // })
 }
 
 module.exports = (send) => {
