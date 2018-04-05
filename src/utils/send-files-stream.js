23f1fc26c50da1724fc1af0d86c1e4d1f77f86cb
David Dias <daviddias.p@gmail.com>
2018-04-05 16:21:52 +0100

    chore: update deps

--- src/utils/send-files-stream.js
+++ src/utils/send-files-stream.js
@@ -78,8 +78,8 @@ module.exports = (send, path) => {
     qs['cid-version'] = propOrProp(options, 'cid-version', 'cidVersion')
     qs['raw-leaves'] = propOrProp(options, 'raw-leaves', 'rawLeaves')
     qs['only-hash'] = propOrProp(options, 'only-hash', 'onlyHash')
-    qs.hash = propOrProp(options, 'hash', 'hashAlg')
     qs['wrap-with-directory'] = propOrProp(options, 'wrap-with-directory', 'wrapWithDirectory')
+    qs.hash = propOrProp(options, 'hash', 'hashAlg')
 
     const args = {
       path: path,
