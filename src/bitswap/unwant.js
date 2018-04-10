2f0bb01501197eb684cdac7584d9962aa799a84d
Michael Garvin <gar+gh@danger.computer>
2018-04-10 09:41:47 -0700

    fix(bitswap.unwant) Parse CID from arg
    CID parsing is done just like block.add does
    This also removes the local tests and moves reliance on testing to the
    tests that are being added to interface-ipfs-core

--- src/bitswap/unwant.js
+++ src/bitswap/unwant.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const promisify = require('promisify-es6')
+const CID = require('cids')
 
 module.exports = (send) => {
   return promisify((args, opts, callback) => {
@@ -8,6 +9,25 @@ module.exports = (send) => {
       callback = opts
       opts = {}
     }
+
+    // Mirrors block.get's parsing of the CID
+    let cid
+    try {
+      if (CID.isCID(args)) {
+        cid = args
+        args = cid.toBaseEncodedString()
+      } else if (Buffer.isBuffer(args)) {
+        cid = new CID(args)
+        args = cid.toBaseEncodedString()
+      } else if (typeof args === 'string') {
+        cid = new CID(args)
+      } else {
+        return callback(new Error('invalid argument'))
+      }
+    } catch (err) {
+      return callback(err)
+    }
+
     send({
       path: 'bitswap/unwant',
       args: args,
