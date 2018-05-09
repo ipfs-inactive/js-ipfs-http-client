ed5b651e529cd3e1c7b69ef38afe32bf0d05926e
Michael Garvin <gar+gh@danger.computer>
2018-05-09 11:39:37 -0700

    feat(bitswap.wantlist) add peer parameter

--- src/bitswap/wantlist.js
+++ src/bitswap/wantlist.js
@@ -1,11 +1,46 @@
 'use strict'
 
 const promisify = require('promisify-es6')
+const CID = require('cids')
 
 module.exports = (send) => {
-  return promisify((callback) => {
+  return promisify((args, opts, callback) => {
+
+    if (typeof (args) === 'function') {
+      callback = args
+      opts = {}
+      args = null
+    } else {
+      if (typeof (opts) === 'function') {
+        callback = opts
+        opts = {}
+      }
+    }
+
+    // Mirrors block.get's parsing of the CID
+    let cid
+    if (args) {
+      try {
+        if (CID.isCID(args)) {
+          cid = args
+          args = cid.toBaseEncodedString()
+        } else if (Buffer.isBuffer(args)) {
+          cid = new CID(args)
+          args = cid.toBaseEncodedString()
+        } else if (typeof args === 'string') {
+          cid = new CID(args)
+        } else {
+          return callback(new Error('invalid argument'))
+        }
+      } catch (err) {
+        return callback(err)
+      }
+      opts.peer = args
+    }
     send({
-      path: 'bitswap/wantlist'
+      path: 'bitswap/wantlist',
+      args: args,
+      qs: opts
     }, callback)
   })
 }
