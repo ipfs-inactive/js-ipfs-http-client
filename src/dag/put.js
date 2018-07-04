cc7a4f4b44f1017035d8a6db3e5473a1e87fe555
Alan Shaw <alan@tableflip.io>
2018-07-04 09:55:18 +0100

    fix(dag): fixes to allow options to be optional
    License: MIT
    Signed-off-by: Alan Shaw <alan@tableflip.io>

--- src/dag/put.js
+++ src/dag/put.js
@@ -7,57 +7,65 @@ const CID = require('cids')
 const multihash = require('multihashes')
 const SendOneFile = require('../utils/send-one-file')
 
-function noop () {}
-
 module.exports = (send) => {
   const sendOneFile = SendOneFile(send, 'dag/put')
 
   return promisify((dagNode, options, callback) => {
     if (typeof options === 'function') {
       callback = options
-    } else if (options.cid && (options.format || options.hash)) {
+    }
+
+    options = options || {}
+
+    if (options.hash) {
+      options.hashAlg = options.hash
+      delete options.hash
+    }
+
+    if (options.cid && (options.format || options.hashAlg)) {
       return callback(new Error('Can\'t put dag node. Please provide either `cid` OR `format` and `hash` options.'))
-    } else if ((options.format && !options.hash) || (!options.format && options.hash)) {
+    } else if ((options.format && !options.hashAlg) || (!options.format && options.hashAlg)) {
       return callback(new Error('Can\'t put dag node. Please provide `format` AND `hash` options.'))
     }
 
-    callback = callback || noop
+    if (options.cid) {
+      let cid
+
+      try {
+        cid = new CID(options.cid)
+      } catch (err) {
+        return callback(err)
+      }
+
+      options.format = cid.codec
+      options.hashAlg = multihash.decode(cid.multihash).name
+      delete options.cid
+    }
 
     const optionDefaults = {
       format: 'dag-cbor',
-      hash: 'sha2-255',
+      hashAlg: 'sha2-256',
       inputEnc: 'raw'
     }
 
-    let hashAlg = options.hash || optionDefaults.hash
-    let format = optionDefaults.format
-    let inputEnc = optionDefaults.inputEnc
+    options = Object.assign(optionDefaults, options)
 
-    if (options.cid && CID.isCID(options.cid)) {
-      format = options.cid.codec
-      hashAlg = multihash.decode(options.cid.multihash).name
-      prepare()
+    if (options.format === 'dag-cbor') {
+      dagCBOR.util.serialize(dagNode, finalize)
+    } else if (options.format === 'dag-pb') {
+      dagPB.util.serialize(dagNode, finalize)
     } else {
-      format = options.format
-      prepare()
-    }
-
-    function prepare () {
-      if (format === 'dag-cbor') {
-        dagCBOR.util.serialize(dagNode, finalize)
-      }
-      if (format === 'dag-pb') {
-        dagPB.util.serialize(dagNode, finalize)
-      }
+      // FIXME Hopefully already serialized...can we use IPLD to serialise instead?
+      finalize(null, dagNode)
     }
 
     function finalize (err, serialized) {
       if (err) { return callback(err) }
       const sendOptions = {
         qs: {
-          hash: hashAlg,
-          format: format,
-          'input-enc': inputEnc
+          hash: options.hashAlg,
+          format: options.format,
+          'input-enc': options.inputEnc
         }
       }
       sendOneFile(serialized, sendOptions, (err, result) => {
