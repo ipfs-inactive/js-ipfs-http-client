29b6d1e2654598d34d1187ae130611fec9fbb2cc
Vasco Santos <vasco.santos@moxy.studio>
2018-08-31 12:21:51 +0100

    feat: ipns over pubsub

---
+++ src/name/pubsub/subs.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const promisify = require('promisify-es6')
+
+const transform = function (res, callback) {
+  callback(null, {
+    strings: res.Strings
+  })
+}
+
+module.exports = (send) => {
+  return promisify((opts, callback) => {
+    if (typeof (opts) === 'function') {
+      callback = opts
+      opts = {}
+    }
+
+    send.andTransform({
+      path: 'name/pubsub/subs',
+      qs: opts
+    }, transform, callback)
+  })
+}
