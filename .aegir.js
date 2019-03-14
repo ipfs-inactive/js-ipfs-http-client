42ec7466a98625bc599ccd11a5737abcd1805be6
Hugo Dias <hugomrdias@gmail.com>
2019-03-14 17:41:41 +0000

    chore: increase bundle size

--- .aegir.js
+++ .aegir.js
@@ -5,7 +5,7 @@ const createServer = require('ipfsd-ctl').createServer
 const server = createServer()
 
 module.exports = {
-  bundlesize: { maxSize: '225kB' },
+  bundlesize: { maxSize: '231kB' },
   webpack: {
     resolve: {
       mainFields: ['browser', 'main']
