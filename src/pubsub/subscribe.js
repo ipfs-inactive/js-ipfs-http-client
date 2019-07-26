3a0fe8a3ca75f67a60b68ece30b02d04c98e3fd7
Alan Shaw <alan.shaw@protocol.ai>
2019-07-26 10:05:33 +0100

    fix: add workaround for subscribe in Firefox
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- src/pubsub/subscribe.js
+++ src/pubsub/subscribe.js
@@ -4,6 +4,7 @@ const ndjson = require('iterable-ndjson')
 const explain = require('explain-error')
 const bs58 = require('bs58')
 const { Buffer } = require('buffer')
+const log = require('debug')('ipfs-http-client:pubsub:subscribe')
 const { objectToQuery } = require('../lib/querystring')
 const configure = require('../lib/configure')
 const { ok, toIterable } = require('../lib/fetch')
@@ -11,6 +12,7 @@ const SubscriptionTracker = require('./subscription-tracker')
 
 module.exports = configure(({ fetch, apiAddr, apiPath, headers }) => {
   const subsTracker = SubscriptionTracker.singleton()
+  const publish = require('./publish')({ fetch, apiAddr, apiPath, headers })
 
   return async (topic, handler, options) => {
     options = options || {}
@@ -25,6 +27,18 @@ module.exports = configure(({ fetch, apiAddr, apiPath, headers }) => {
     const url = `${apiAddr}${apiPath}/pubsub/sub${qs}`
     let res
 
+    // In Firefox, the initial call to fetch does not resolve until some data
+    // is received. If this doesn't happen within 1 second send an empty message
+    // to kickstart the process.
+    const ffWorkaround = setTimeout(async () => {
+      log(`Publishing empty message to "${topic}" to resolve subscription request`)
+      try {
+        await publish(topic, Buffer.alloc(0), options)
+      } catch (err) {
+        log('Failed to publish empty message', err)
+      }
+    }, 1000)
+
     try {
       res = await ok(fetch(url, {
         method: 'POST',
@@ -36,6 +50,8 @@ module.exports = configure(({ fetch, apiAddr, apiPath, headers }) => {
       throw err
     }
 
+    clearTimeout(ffWorkaround)
+
     readMessages(ndjson(toIterable(res.body)), {
       onMessage: handler,
       onEnd: () => subsTracker.unsubscribe(topic, handler),
@@ -45,8 +61,7 @@ module.exports = configure(({ fetch, apiAddr, apiPath, headers }) => {
 })
 
 async function readMessages (msgStream, { onMessage, onEnd, onError }) {
-  // eslint-disable-next-line no-console
-  onError = onError || (err => console.error(err))
+  onError = onError || log
 
   try {
     for await (const msg of msgStream) {
@@ -58,7 +73,7 @@ async function readMessages (msgStream, { onMessage, onEnd, onError }) {
           topicIDs: msg.topicIDs
         })
       } catch (err) {
-        onError(explain(err, 'Failed to parse pubsub message'), false) // Not fatal
+        onError(explain(err, 'Failed to parse pubsub message'), false, msg) // Not fatal
       }
     }
   } catch (err) {
