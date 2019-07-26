b346479accb530bd14896b8c6e58056ab806ba06
Alan Shaw <alan.shaw@protocol.ai>
2019-07-26 17:09:28 +0100

    refactor: use promise-nodeify
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- src/pubsub/index.js
+++ src/pubsub/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const callbackify = require('../lib/callbackify')
+const nodeify = require('promise-nodeify')
 
 // This file is temporary and for compatibility with legacy usage
 module.exports = (send, options) => {
@@ -8,11 +8,43 @@ module.exports = (send, options) => {
     options = send
   }
 
+  const ls = require('./ls')(options)
+  const peers = require('./peers')(options)
+  const publish = require('./publish')(options)
+  const subscribe = require('./subscribe')(options)
+  const unsubscribe = require('./unsubscribe')(options)
+
   return {
-    ls: callbackify(require('./ls')(options)),
-    peers: callbackify(require('./peers')(options)),
-    publish: callbackify(require('./publish')(options)),
-    subscribe: callbackify(require('./subscribe')(options), { minArgs: 2 }),
-    unsubscribe: callbackify(require('./unsubscribe')(options), { minArgs: 2 })
+    ls: (options, callback) => {
+      if (typeof options === 'function') {
+        callback = options
+        options = {}
+      }
+      return nodeify(ls(options), callback)
+    },
+    peers: (topic, options, callback) => {
+      if (typeof options === 'function') {
+        callback = options
+        options = {}
+      }
+      return nodeify(peers(topic, options), callback)
+    },
+    publish: (topic, data, options, callback) => {
+      if (typeof options === 'function') {
+        callback = options
+        options = {}
+      }
+      return nodeify(publish(topic, data, options), callback)
+    },
+    subscribe: (topic, handler, options, callback) => {
+      if (typeof options === 'function') {
+        callback = options
+        options = {}
+      }
+      return nodeify(subscribe(topic, handler, options), callback)
+    },
+    unsubscribe: (topic, handler, callback) => {
+      return nodeify(unsubscribe(topic, handler), callback)
+    }
   }
 }
