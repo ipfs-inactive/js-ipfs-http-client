70653adff2eb65c528b2fa88ed7172218253c5ca
Alan Shaw <alan.shaw@protocol.ai>
2019-07-30 11:53:22 +0100

    refactor: use ky
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- src/pubsub/unsubscribe.js
+++ src/pubsub/unsubscribe.js
@@ -3,7 +3,7 @@
 const configure = require('../lib/configure')
 const SubscriptionTracker = require('./subscription-tracker')
 
-module.exports = configure(({ fetch, apiAddr, apiPath, headers }) => {
+module.exports = configure(({ ky }) => {
   const subsTracker = SubscriptionTracker.singleton()
   // eslint-disable-next-line require-await
   return async (topic, handler) => subsTracker.unsubscribe(topic, handler)
