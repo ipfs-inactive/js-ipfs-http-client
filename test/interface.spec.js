0e7140d7849eea721a765056d26cca38a5dc8e87
Hugo Dias <hugomrdias@gmail.com>
2019-09-15 08:52:48 +0100

    fix: enable more tests

--- test/interface.spec.js
+++ test/interface.spec.js
@@ -54,6 +54,7 @@ describe('interface-ipfs-core tests', () => {
   })
 
   tests.dag(defaultCommonFactory, {
+    only: true,
     skip: [
       // dag.tree
       {
@@ -187,11 +188,7 @@ describe('interface-ipfs-core tests', () => {
   })
 
   tests.miscellaneous(defaultCommonFactory, {
-    only: [
-      {
-        name: 'should get the node version'
-      }
-    ],
+    only: true,
     skip: [
       // stop
       {
