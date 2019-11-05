fdac3e88016c828e66ea9832bbd95d540cde4a9d
Marcin Rataj <lidel@lidel.org>
2019-11-05 20:49:57 +0100

    feat: support name.resolve of peerid as cid
    This is a hacky way to support CIDs when API is go-ipfs 0.4.22 or older
    (convert toBase58btc before sending)
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- src/name/resolve.js
+++ src/name/resolve.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const promisify = require('promisify-es6')
+const CID = require('cids')
+const multihash = require('multihashes')
 
 const transform = function (res, callback) {
   callback(null, res.Path)
@@ -11,6 +13,19 @@ module.exports = (send) => {
     if (typeof (opts) === 'function') {
       callback = opts
       opts = {}
+    }
+
+    // normalize PeerIDs to Base58btc, so it works with go-ipfs <=0.4.22
+    if (typeof (args) === 'string') {
+      try {
+        const path = args.split('/')
+        if (path.length > 2) {
+          path[2] = multihash.toB58String(new CID(path[2]).multihash)
+          args = path.join('/')
+        }
+      } catch (err) {
+        // noop
+      }
     }
 
     send.andTransform({
