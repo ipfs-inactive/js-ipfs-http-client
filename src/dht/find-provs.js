c11fd4ca9b1a7e05ea6a8e2162baae12a503bfd2
Alan Shaw <alan.shaw@protocol.ai>
2019-11-14 12:09:19 +0000

    feat: convert dht.get to async/await

--- src/dht/find-provs.js
+++ src/dht/find-provs.js
@@ -4,9 +4,9 @@ const PeerId = require('peer-id')
 const PeerInfo = require('peer-info')
 const multiaddr = require('multiaddr')
 const ndjson = require('iterable-ndjson')
+const log = require('debug')('ipfs-http-client:dht:find-provs')
 const configure = require('../lib/configure')
 const toIterable = require('../lib/stream-to-iterable')
-const toCamel = require('../lib/object-to-camel')
 
 module.exports = configure(({ ky }) => {
   return (cid, options) => (async function * () {
@@ -24,16 +24,17 @@ module.exports = configure(({ ky }) => {
       searchParams
     })
 
-    for await (let message of ndjson(toIterable(res.body))) {
-      message = toCamel(message)
-      if (message.responses) {
-        message.responses = message.responses.map(({ ID, Addrs }) => {
+    for await (const message of ndjson(toIterable(res.body))) {
+      log(message)
+      // 4 = Provider
+      // https://github.com/libp2p/go-libp2p-core/blob/6e566d10f4a5447317a66d64c7459954b969bdab/routing/query.go#L20
+      if (message.Type === 4 && message.Responses) {
+        for (const { ID, Addrs } of message.Responses) {
           const peerInfo = new PeerInfo(PeerId.createFromB58String(ID))
           if (Addrs) Addrs.forEach(addr => peerInfo.multiaddrs.add(multiaddr(addr)))
-          return peerInfo
-        })
+          yield peerInfo
+        }
       }
-      yield message
     }
   })()
 })
--- src/dht/index.js
+++ src/dht/index.js
@@ -2,34 +2,27 @@
 
 const callbackify = require('callbackify')
 const errCode = require('err-code')
+const { collectify } = require('../lib/converters')
 const moduleConfig = require('../utils/module-config')
 
 module.exports = (arg, config) => {
   const send = moduleConfig(arg)
+  const get = require('./get')(config)
   const findProvs = require('./find-provs')(config)
   const findPeer = require('./find-peer')(config)
 
   return {
-    get: require('./get')(send),
-    put: require('./put')(send),
-    findProvs: callbackify.variadic(async (cid, options) => {
-      const providers = []
-      for await (const message of findProvs(cid, options)) {
-        // 4 = Provider
-        // https://github.com/libp2p/go-libp2p-core/blob/6e566d10f4a5447317a66d64c7459954b969bdab/routing/query.go#L20
-        if (message.type === 4) {
-          providers.push(...message.responses)
-        }
+    get: callbackify.variadic(async (key, options) => {
+      for await (const value of get(key, options)) {
+        return value
       }
-      return providers
+      throw errCode(new Error('value not found'), 'ERR_TYPE_5_NOT_FOUND')
     }),
+    put: require('./put')(send),
+    findProvs: callbackify.variadic(collectify(findProvs)),
     findPeer: callbackify.variadic(async (peerId, options) => {
-      for await (const message of findPeer(peerId, options)) {
-        // 2 = FinalPeer
-        // https://github.com/libp2p/go-libp2p-core/blob/6e566d10f4a5447317a66d64c7459954b969bdab/routing/query.go#L18
-        if (message.type === 2) {
-          return message.responses[0]
-        }
+      for await (const peerInfo of findPeer(peerId, options)) {
+        return peerInfo
       }
       throw errCode(new Error('final peer not found'), 'ERR_TYPE_2_NOT_FOUND')
     }),
