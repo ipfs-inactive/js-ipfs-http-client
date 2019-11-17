7242c72bd92b015d49401e1bd5009e83795ab1ce
Alan Shaw <alan.shaw@protocol.ai>
2019-11-17 22:41:41 +0000

    feat: use it-tar

--- src/utils/tar-stream-to-objects.js
+++ src/utils/tar-stream-to-objects.js
@@ -1,32 +1,7 @@
 'use strict'
 
-const tar = require('tar-stream')
-const { EventIterator } = require('event-iterator')
-
-function pipe (reader, writable) {
-  reader.read()
-    .then(({ done, value }) => {
-      if (done) {
-        writable.end()
-
-        return
-      }
-
-      if (value) {
-        const beneathHighWaterMark = writable.write(value)
-
-        if (beneathHighWaterMark) {
-          pipe(reader, writable)
-        } else {
-          writable.once('drain', () => {
-            pipe(reader, writable)
-          })
-        }
-      }
-    }, (err) => {
-      writable.emit('error', err)
-    })
-}
+const tar = require('it-tar')
+const toIterable = require('../lib/stream-to-iterable')
 
 /*
   Transform a tar readable stream into an async iterator of objects:
@@ -35,40 +10,9 @@ function pipe (reader, writable) {
   { path: 'string', content: AsyncIterator<Buffer> }
 */
 async function * tarStreamToObjects (inputStream) {
-  const extractStream = tar.extract()
-  let onEntry
+  const extractor = tar.extract()
 
-  const tarStream = new EventIterator(
-    (push, stop, fail) => {
-      onEntry = (header, stream, next) => {
-        push({ header, stream })
-
-        next()
-      }
-
-      extractStream.addListener('entry', onEntry)
-      extractStream.addListener('finish', stop)
-      extractStream.addListener('error', fail)
-    },
-    (push, stop, fail) => {
-      extractStream.removeListener('entry', onEntry)
-      extractStream.removeListener('finish', stop)
-      extractStream.removeListener('error', fail)
-      extractStream.destroy()
-    }
-  )
-
-  if (inputStream.pipe) {
-    // node stream
-    inputStream.pipe(extractStream)
-  } else if (inputStream.getReader) {
-    // browser readable stream
-    pipe(inputStream.getReader(), extractStream)
-  } else {
-    throw new Error('Unknown stream type')
-  }
-
-  for await (const { header, stream } of tarStream) {
+  for await (const { header, body } of extractor(toIterable(inputStream))) {
     if (header.type === 'directory') {
       yield {
         path: header.name
@@ -76,7 +20,11 @@ async function * tarStreamToObjects (inputStream) {
     } else {
       yield {
         path: header.name,
-        content: stream
+        content: (async function * () {
+          for await (const chunk of body) {
+            yield chunk.slice() // Convert BufferList to Buffer
+          }
+        })()
       }
     }
   }
