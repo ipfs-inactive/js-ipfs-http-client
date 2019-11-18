8041632159f7b2d3f99133a650f6b5af966916b8
Pedro Santos <pedro.santos@moxy.studio>
2019-11-18 16:01:54 +0000

    feat: add 'human' option and format output

--- src/bitswap/stat.js
+++ src/bitswap/stat.js
@@ -7,27 +7,44 @@ module.exports = configure(({ ky }) => {
   return async (options) => {
     options = options || {}
 
+    const searchParams = new URLSearchParams(options.searchParams)
+    if (options.human) searchParams.set('human', options.human)
+
     const res = await ky.get('bitswap/stat', {
       timeout: options.timeout,
       signal: options.signal,
       headers: options.headers,
-      searchParams: options.searchParams
+      searchParams
     }).json()
 
-    return toCoreInterface(res)
+    return toCoreInterface(res, options)
   }
 })
 
-function toCoreInterface (res) {
+function toCoreInterface (res, { human }) {
   return {
-    provideBufLen: res.ProvideBufLen,
+    provideBufLen: human
+      ? res.ProvideBufLen
+      : new Big(res.ProvideBufLen),
     wantlist: res.Wantlist || [],
     peers: res.Peers || [],
-    blocksReceived: new Big(res.BlocksReceived),
-    dataReceived: new Big(res.DataReceived),
-    blocksSent: new Big(res.BlocksSent),
-    dataSent: new Big(res.DataSent),
-    dupBlksReceived: new Big(res.DupBlksReceived),
-    dupDataReceived: new Big(res.DupDataReceived)
+    blocksReceived: human
+      ? res.BlocksReceived
+      : new Big(res.BlocksReceived),
+    dataReceived: human
+      ? res.DataReceived
+      : new Big(res.DataReceived),
+    blocksSent: human
+      ? res.BlocksSent
+      : new Big(res.BlocksSent),
+    dataSent: human
+      ? res.DataSent
+      : new Big(res.DataSent),
+    dupBlksReceived: human
+      ? res.DupBlksReceived
+      : new Big(res.DupBlksReceived),
+    dupDataReceived: human
+      ? res.DupDataReceived
+      : new Big(res.DupDataReceived)
   }
 }
